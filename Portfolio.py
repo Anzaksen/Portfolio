cc2ecb9a0ce15177530f95c070b7b8b9ebb432ba
Anzaksen <104062652+Anzaksen@users.noreply.github.com>
2024-10-07 13:23:29 +0100

    Add files via upload

---
+++ Portfolio.py
@@ -0,0 +1,107 @@
+import re
+
+import streamlit as st
+import requests
+
+from forms.contact import contact_form
+
+WEBHOOK_URL = st.secrets["WEBHOOK_URL"]
+
+
+def is_valid_email(email):
+    # Basic regex pattern for email validation
+    email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return re.match(email_pattern, email) is not None
+
+
+def contact_form():
+    with st.form("contact_form"):
+        name = st.text_input("First Name")
+        email = st.text_input("Email Address")
+        message = st.text_area("Your Message")
+        submit_button = st.form_submit_button("Submit")
+
+    if submit_button:
+        if not WEBHOOK_URL:
+            st.error("Email service is not set up. Please try again later.", icon="📧")
+            st.stop()
+
+        if not name:
+            st.error("Please provide your name.", icon="🧑")
+            st.stop()
+
+        if not email:
+            st.error("Please provide your email address.", icon="📨")
+            st.stop()
+
+        if not is_valid_email(email):
+            st.error("Please provide a valid email address.", icon="📧")
+            st.stop()
+
+        if not message:
+            st.error("Please provide a message.", icon="💬")
+            st.stop()
+
+        # Prepare the data payload and send it to the specified webhook URL
+        data = {"email": email, "name": name, "message": message}
+        response = requests.post(WEBHOOK_URL, json=data)
+
+        if response.status_code == 200:
+            st.success("Your message has been sent successfully! 🎉", icon="🚀")
+        else:
+            st.error("There was an error sending your message.", icon="😨")
+
+
+@st.experimental_dialog("Contact Me")
+def show_contact_form():
+    contact_form()
+
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+with col1:
+    #st.image("./assets/profile_image.png", width=230)
+
+with col2:
+    st.title("Dape Alexander Naanret", anchor=False)
+    st.write(
+        "Data Analyst,  interested in digging through data and uncovering meaningful insights."
+    )
+    if st.button("✉️ Contact Me"):
+        show_contact_form()
+
+
+# --- EXPERIENCE & QUALIFICATIONS ---
+st.write("\n")
+st.subheader("Projects", anchor=False)
+st.write(
+    """
+    - 7 Years experience extracting actionable insights from data
+    - Strong hands-on experience and knowledge in Python and Excel
+    - Good understanding of statistical principles and their respective applications
+    - Excellent team-player and displaying a strong sense of initiative on tasks
+    """
+)
+
+# --- SKILLS ---
+st.write("\n")
+st.subheader("Hard Skills", anchor=False)
+st.write(
+    """
+    - Programming: Python (Scikit-learn, Pandas, e.t.c), SQL.
+    - Data Visualization: PowerBi, , Plotly.
+    - Modeling: Machine Learning, Deep learning
+    """
+)
+
+
+# --- About ---
+st.write("\n")
+st.subheader("My Personality & Work Ethic", anchor=False)
+st.write(
+    """
+   I am a hard worker and I am always willing to go the extra mile to get the job done. 
+   I am a hard worker, and also a highly motivated and results-oriented individual. 
+   I am eager to learn more about your company and the role that I can play in its success.
+    I am confident that I can be a valuable asset to your team."""
+)
