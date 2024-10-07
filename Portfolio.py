a3bd88787c0b1153b05c9c95e4805833cb528811
Anzaksen <104062652+Anzaksen@users.noreply.github.com>
2024-10-07 13:38:59 +0100

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -60,7 +60,7 @@ def show_contact_form():
 # --- HERO SECTION ---
 col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
 with col1:
-    #st.image("./assets/profile_image.png", width=230)
+    st.image("./profile_image.jpg", width=230)
 
 with col2:
     st.title("Dape Alexander Naanret", anchor=False)
